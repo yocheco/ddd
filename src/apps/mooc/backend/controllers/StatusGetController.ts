e5727fd77dcb16d1d77384d294bfdb59fa664b16
Sergio Rogelio Sandoval <yo.sergio.sandoval@gmail.com>
2022-01-03 13:50:27 -0600

    :test: -Test commit

--- src/apps/mooc/backend/controllers/StatusGetController.ts
+++ src/apps/mooc/backend/controllers/StatusGetController.ts
@@ -4,6 +4,6 @@ import { Controller } from './Controller';
 
 export default class StatusGetController implements Controller {
   async run(req: Request, res: Response) {
-    res.status(httpStatus.OK).send();
+    res.status(200).send();
   }
 }
